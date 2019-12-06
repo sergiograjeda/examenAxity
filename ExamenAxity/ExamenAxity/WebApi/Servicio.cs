4e6b44fd0d6f337358ce0e9badea3f9bd84a514f
sergiograjeda <58582227+sergiograjeda@users.noreply.github.com>
2019-12-05 23:35:51 -0600

    funciones
    Generar un servicio web del tipo rest (webapi) que contenga dos métodos:

---
+++ ExamenAxity/ExamenAxity/WebApi/Servicio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ExamenAxity.Context;
+
+namespace ExamenAxity.WebApi
+{
+    public class Servicio : ApiController
+    {
+        public bool Login(string Usuario, string Password)
+        {
+            var respuesta = false;
+            using (ExamAxityEntities db = new ExamAxityEntities())
+            {
+                var consulta = db.Users.Where(s => s.user == Usuario && s.pwd == Password);
+                if (consulta.Any())
+                    respuesta = true;
+            }
+            return respuesta;
+        }
+
+        [HttpGet]
+        public List<Products> GetProductos()
+        {
+            var respuesta = new List<Products>();
+            using (ExamAxityEntities db = new ExamAxityEntities())
+            {
+                var consulta = db.Products.ToList();
+                if (consulta.Any())
+                    return consulta;
+            }
+            return respuesta;
+        }
+    }
+}
